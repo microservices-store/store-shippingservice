639de3f3c0e779fb053e2ce904a90cf7164bc8bd
Steve Taylor <steve@deployhub.com>
2022-02-08 15:42:28 -0700

    debug cloudbuild

---
+++ setup.sh
@@ -0,0 +1,20 @@
+
+export BLDDATE="Tue Feb  8 22:04:42 UTC 2022"
+export BUILDER_OUTPUT=/builder/outputs
+export BUILDNUM=67
+export DHURL=https://console.deployhub.com
+export DOCKER_CONFIG=/workspace/docker-config
+export GIT_REPO=ortelius/store-shippingservice
+export GIT_URL=https://github.com/ortelius/store-shippingservice.git
+export GPG_KEY=A035C8C19219BA821ECEA86B64E628F8D684696D
+export HOME=/builder/home
+export HOSTNAME=9d805c48ec07
+export IMAGE_TAG=-v1.2.2.67-g
+export LANG=C.UTF-8
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PWD=/workspace
+export PYTHON_GET_PIP_SHA256=7c5239cea323cadae36083079a5ee6b2b3d56f25762a0c060d2867b89e5e06c5
+export PYTHON_GET_PIP_URL=https://github.com/pypa/get-pip/raw/2caf84b14febcda8077e59e9b8a6ef9a680aa392/public/get-pip.py
+export PYTHON_PIP_VERSION=21.2.4
+export PYTHON_SETUPTOOLS_VERSION=58.1.0
+export PYTHON_VERSION=3.10.2
